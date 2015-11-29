66205c7b6ee7b196bdda63994d22b4d9fac03778
HAROLD <havilez@gmail.com>
2015-11-28 20:47:59 -0500

    updated production section to use the correct uri and admin user for mongodb

--- config/env/production.js
+++ config/env/production.js
@@ -8,10 +8,11 @@ module.exports = {
   },
   port: process.env.PORT || 8443,
   db: {
-    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean',
+    uri: 'mongodb://nvpcoodes:sankofa123>@ds059524.mongolab.com:59524/mean-nvpcodes',
+      //uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean',
     options: {
-      user: '',
-      pass: ''
+      user: 'nvpcodes',
+      pass: 'sankofa123'
     },
     // Enable mongoose debug mode
     debug: process.env.MONGODB_DEBUG || false
